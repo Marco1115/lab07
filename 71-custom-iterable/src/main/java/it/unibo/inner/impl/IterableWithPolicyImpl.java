eb4ceb8d4f29c54790fec64e162ecb5563321006
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-03 11:49:23 +0100

    Implemented Iterator method and ArrayIterator inner class

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,22 +1,53 @@
 package it.unibo.inner.impl;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
 
 import it.unibo.inner.api.IterableWithPolicy;
 import it.unibo.inner.api.Predicate;
 
 class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
+    private final List<T> elements;
+
+    public IterableWithPolicyImpl(final T[] inputArray) {
+        this.elements = new ArrayList<>(Arrays.asList(inputArray));
+    }
+
     @Override
     public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+        return this.new ArrayIterator<T>();
     }
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+        // TODO
+    }
+
+    private class ArrayIterator<T> implements Iterator<T> {
+
+        private int current;
+
+        public ArrayIterator() {
+            this.current = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (this.current < IterableWithPolicyImpl.this.elements.size() - 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public T next() {
+            return (T) IterableWithPolicyImpl.this.elements.get(current++);
+        }
+
     }
 
 }
