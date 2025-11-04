c1cd26c3e9680f2916c3a1afdf3ebbb2e1791635
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-04 20:54:14 +0100

    implemented static method transform of the Transformers class

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,11 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        final List<O> out = new ArrayList<>();
+        for (I elem: base) {
+            out.addLast(transformer.call(elem));
+        }
+        return out;
     }
 
     /**
