c50855b5d605359baf4d2f3cca4d35a3f262d2f6
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-08 18:45:21 +0100

    Finished the implementation of the third part of exercise 72

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,6 +1,7 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 /**
  * Implementation of {@link MonthSorter}.
@@ -9,15 +10,15 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
     }
 
-    enum Month {
+    static enum Month {
 
         JANUARY(31),
         FEBRUARY(28),
@@ -36,6 +37,45 @@ public final class MonthSorterNested implements MonthSorter {
 
         private Month(final int days) {
             this.monthDaysNumber = days;
+        }
+
+        public static Month fromString(final String monthName) {
+            boolean foundMonth = false;
+            Month result = null;
+            Objects.requireNonNull(monthName);
+            for(final Month month: Month.values()) {
+                if(month.name().toLowerCase().startsWith(monthName.toLowerCase())) {
+                    if(!foundMonth) {
+                        result = month;
+                        foundMonth = true;
+                    } else {
+                        throw new IllegalArgumentException("Ambiguos month name");
+                    }
+                }
+            }
+            if(foundMonth) {
+                return result;
+            } else {
+                throw new IllegalArgumentException("No correnponding month name");
+            }
+        }
+
+        public int getMonthDaysNumber() {
+            return monthDaysNumber;
+        }
+    }
+
+    class SortByMonthOrder implements Comparator<String> {
+        @Override
+        public int compare(String monthName0, String monthName1) {
+            return Month.fromString(monthName0).compareTo(Month.fromString(monthName1));    
+        }
+    }
+
+    class SortByDate implements Comparator<String> {
+        @Override
+        public int compare(String monthName0, String monthName1) {
+            return Integer.compare(Month.fromString(monthName0).getMonthDaysNumber(), Month.fromString(monthName1).getMonthDaysNumber());
         }
 
     }
